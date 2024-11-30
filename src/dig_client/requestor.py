6129f67646737885f82e88fdca43337dfbb282a5
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2024-11-30 10:46:29 +0800

    Update requestor.py

--- src/dig_client/requestor.py
+++ src/dig_client/requestor.py
@@ -1,8 +1,9 @@
+import asyncio
 import httpx
-
 from . import config
 
 
+semaphore = asyncio.Semaphore(512)
 client = httpx.AsyncClient(timeout=3600, limits=httpx.Limits(max_connections=512))
 
 
@@ -12,10 +13,20 @@ async def request_image_generation(prompt: str, task_id: str = None, seed: int =
         extra_args["task_id"] = task_id
     if seed is not None:
         extra_args["seed"] = seed
-    response = await client.post(
-        f"{config.SERVER_URL}/request",
-        json={"prompt": prompt, "extra_args": extra_args},
-    )
+    async with semaphore:
+        for _ in range(5):
+            try:
+                response = await client.post(
+                    f"{config.SERVER_URL}/request",
+                    json={"prompt": prompt, "extra_args": extra_args},
+                )
+                break
+            except Exception as e:
+                await asyncio.sleep(1)
+                continue
+        else:
+            print(e)
+            return None
     if response.status_code == 200:
         task_id = response.json()["task_id"]
         # print(f"Task created with ID: {task_id}")
